6126edc30d927b48b796a42530c2d4cd64617f16
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-04-02 18:28:23 +0500

    Musor commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Public from './routes/Public'
 import Private from './routes/Private'
-
+import './App.scss'
 import Login from './pages/Login/Login'
 import Home from './pages/Home/Home'
 import CreateBook from './pages/CreateBook/CreateBook'
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider as Authentication } from './context/Authentication'
+import './index.scss'
 
 ReactDOM.render(
     <React.StrictMode>
